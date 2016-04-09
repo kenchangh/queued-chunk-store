f5700d2a236aa6e6b9017cfeae12cff5751fc8bb
Chan Guan Hao <guanhao3797@gmail.com>
2016-04-10 00:53:07 +0800

    more test

--- test.js
+++ test.js
@@ -30,7 +30,7 @@ test('get before put', function (t) {
 })
 
 test('put then get', function (t) {
-  var store = new Queued(Immediate(MemoryChunkStore(10)))
+  var store = new Queued(MemoryChunkStore(10))
 
   store.put(0, new Buffer('0123456789'), function() {
     store.get(0, function(err, buf) {
@@ -62,6 +62,43 @@ test('gets should be removed after put', function (t) {
       t.end()
     })
   })
+})
+
+test('multiple put', function (t) {
+  var store = new Queued(new MemoryChunkStore(10))
+
+  store.get(0, function(err, buf) {
+    t.error(err)
+    t.deepEqual(buf, new Buffer('0123456789'))
+    didGet0 = true
+    maybeDone()
+  })
+
+  store.get(1, function(err, buf) {
+    t.error(err)
+    t.deepEqual(buf, new Buffer('1234567890'))
+    didGet1 = true
+    maybeDone()
+  })
+
+  store.put(0, new Buffer('0123456789'), function() {
+    t.equal(store.queuedGets[0].length, 0)
+  })
+
+  store.put(1, new Buffer('1234567890'), function() {
+    t.equal(store.queuedGets[1].length, 0)
+  })
+
+  var didGet0 = false
+  var didGet1 = false
+  function maybeDone() {
+    if (didGet0 && didGet1) {
+      store.destroy(function (err) {
+        t.error(err)
+        t.end()
+      })
+    }
+  }
 })
 
 test('get should work after put', function (t) {
